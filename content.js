7ae9f952d980234a2c7de178e6fb33ae2b17e170
Yasser Qureshi <yasser.q@hotmail.com>
2021-11-20 23:43:59 +0000

    Update new ids

--- content.js
+++ content.js
@@ -4,15 +4,15 @@ chrome.runtime.onMessage.addListener(
             document.getElementById("order_billing_name").value = request.name;
             document.getElementById("order_email").value = request.email;
             document.getElementById("order_tel").value = request.telephone;
-            document.getElementById("bo").value = request.address1;
-            document.getElementById("oba3").value = request.address2;
+            document.getElementById("order_billing_address").value = request.address1;
+            document.getElementById("order_billing_address_2").value = request.address2;
             document.getElementById("order_billing_address_3").value = request.address3;
             document.getElementById("order_billing_city").value = request.city;
             document.getElementById("order_billing_zip").value = request.postcode;
-            document.getElementById("cnb").value = request.cardnumber;
+            document.getElementById("credit_card_number").value = request.cardnumber;
             document.getElementById('credit_card_month').value = request.month;
             document.getElementById('credit_card_year').value = request.year;
-            document.getElementById("vval").value = request.cvv; 
+            document.getElementById("credit_card_verification_value").value = request.cvv; 
             sendResponse({status: "Success!"});
         } catch (error) {
             console.log(error)
